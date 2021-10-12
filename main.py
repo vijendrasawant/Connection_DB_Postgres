b05395d47d6ea58f4d9e4e96e4dd49334355db3f
vijendrasawant <vijendrasawant@hotmail.com>
2021-10-12 18:40:59 +0530

    Initial Load-v1

--- main.py
+++ main.py
@@ -7,6 +7,29 @@ import psycopg2
 from configparser import ConfigParser
 
 def config(filename='config.ini', section='Database'):
+    #create a parser
+    parser = ConfigParser()
+    #read config file
+    parser.read(filename)
+
+    #get section, defaults to Database
+    db = {}
+
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = params[1]
+    else:
+        raise Exception('Section {0] not found in the {1} file'.format(section,filename))
+
+    return db
+
+
+
+
+
+
+
 
 
 
