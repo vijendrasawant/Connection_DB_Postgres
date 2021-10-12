5032a54c60db13455fb6695355ba50d51f660b7a
vijendrasawant <vijendrasawant@hotmail.com>
2021-10-12 18:19:19 +0530

    Initial Load

---
+++ main.py
@@ -0,0 +1,12 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import psycopg2
+from configparser import ConfigParser
+
+def config(filename='config.ini', section='Database'):
+
+
+
